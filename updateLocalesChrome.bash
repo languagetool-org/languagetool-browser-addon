4b85c68af17c1820329a6c993d20aba7325a5897
Daniel Naber <naber@danielnaber.de>
2015-11-02 21:43:18 +0100

    add script to fetch translated JSON files for Chrome from Transifex

---
+++ updateLocalesChrome.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# script to fetch translated JSON files for Chrome from Transifex
+
+source .lgn
+
+if [[ -z "$U" || -z "$P" ]]; then
+	echo "Set \$U and \$P with username and password for transifex"
+	exit
+fi
+
+for lang in `ls chrome/_locales | sed "s/en//g"`; do
+	echo "Getting $lang..."
+	curl --user $U:$P http://www.transifex.net/api/2/project/languagetool/resource/chrome-extension/translation/$lang/?file > chrome/_locales/$lang/messages.json
+done
