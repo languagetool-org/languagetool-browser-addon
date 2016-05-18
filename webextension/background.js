94c9afe2d638d56815f2cdd838dcc9d458a9d9e2
Daniel Naber <naber@danielnaber.de>
2016-05-18 16:49:20 +0200

    remove the "reminder toolbar" code, which has never been active yet anyway

--- webextension/background.js
+++ webextension/background.js
@@ -20,19 +20,3 @@ if (chrome && chrome.browserAction && chrome.browserAction.openPopup) {
     //chrome.contextMenus.create({"title": "Check text field", "contexts":["editable"], "id": "contextLTeditable"});
   });
 }
-
-function toggleToolbar() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, "toggle-in-page-toolbar");
-  });
-}
-
-chrome.browserAction.onClicked.addListener(toggleToolbar);
-
-// Handle connections received from the add-on toolbar ui iframes.
-chrome.runtime.onConnect.addListener(function (port) {
-  if (port.sender.url == chrome.runtime.getURL("toolbar/ui.html")) {
-    // Handle port messages received from the connected toolbar ui frames.
-    port.onMessage.addListener(toggleToolbar);
-  }
-});
--- webextension/toolbar/ui.js
+++
@@ -1,6 +0,0 @@
-var port = chrome.runtime.connect();
-
-document.querySelector("#toggle").addEventListener("click", function() {
-  // Ask the background page to toggle the toolbar on the current tab
-  port.postMessage("toggle-in-page-toolbar");
-});
