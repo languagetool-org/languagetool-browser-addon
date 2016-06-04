9d194e8cb6bbc4be80188b31c05b5e737ea53971
Daniel Naber <naber@danielnaber.de>
2016-06-04 16:36:18 +0200

    fix uninstall URL

--- webextension/background.js
+++ webextension/background.js
@@ -1,5 +1,5 @@
 
-chrome.runtime.setUninstallURL("https://languagetool.org/chrome/uninstall.html");
+chrome.runtime.setUninstallURL("https://languagetool.org/chrome/uninstall.php");
 
 function onClickHandler(info, tab) {
   if (chrome && chrome.browserAction && chrome.browserAction.openPopup) {
