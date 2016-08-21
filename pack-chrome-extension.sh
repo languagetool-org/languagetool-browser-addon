97b96b831f893e0f0a106ea0a36a3c60a76cfbcc
Daniel Naber <naber@danielnaber.de>
2016-08-21 22:32:25 +0200

    rename

--- pack-chrome-extension.sh
+++ pack-chrome-extension.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# pack everything as an extension
+# pack everything as a Chrome extension
  
 cd /lt/git/languagetool-browser-addon/webextension
 TARGET=~/languagetool-webextension-chrome.zip
 rm -i $TARGET
-zip -x tests.js -x test/ -x test/test.js -x languagetool-for-chrome-tests.html -r $TARGET .
+zip -x tests.js -x test/ -x test/\* -x languagetool-for-chrome-tests.html -r $TARGET .
 echo "Saved to $TARGET"
 cd -
