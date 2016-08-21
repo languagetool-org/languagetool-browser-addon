b04e09606f4e3505e30c3a23eb34452e5e8e8c18
Daniel Naber <naber@danielnaber.de>
2016-08-21 22:26:42 +0200

    update file name

--- pack.sh
+++ pack.sh
@@ -2,7 +2,7 @@
 # pack everything as an extension
  
 cd /lt/git/languagetool-browser-addon/webextension
-TARGET=~/languagetool-webextension.zip
+TARGET=~/languagetool-webextension-chrome.zip
 rm -i $TARGET
 zip -x tests.js -x test/ -x test/test.js -x languagetool-for-chrome-tests.html -r $TARGET .
 echo "Saved to $TARGET"
