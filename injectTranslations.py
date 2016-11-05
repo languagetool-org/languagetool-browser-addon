d85ff9b5279872b07ecbd99c67d01627864e52e7
edawine <sutanto.maxedwin@gmail.com>
2016-11-05 10:44:50 +0700

    Minor changes (replace #!, use 'with', '.format')
    Replaces hardcoded shebang with 'env' to be more flexible
    Uses 'with' statement in loadLanguageDict function to ensure file closes after the value is returned
    Changes '+' in strings with '.format()' so that if something is to be added or removed, it is easier

--- injectTranslations.py
+++ injectTranslations.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Daniel Naber, 2015-11-04
 # Created a JSON file based on the existing English file,
@@ -11,47 +11,53 @@ import re
 import sys
 import collections
 
+
 def loadLanguageDict(filename):
     codeToLang = collections.OrderedDict()
-    file = open(filename)
-    for line in file:
-        regex = re.compile("([a-z][a-z]|[a-z][a-z]-[A-Z][A-Z]|[a-z][a-z][a-z]|[a-z][a-z]-[A-Z][A-Z]-.*?)\\s*=\\s*(.*)")  # e.g. "de", "de-DE", "ast", "ca-ES-valencia"
-        match = regex.match(line)
-        if match:
-            codeToLang[match.group(1)] = match.group(2)
-    return codeToLang
+    with open(filename) as File:
+        for line in File:
+            regex = re.compile("([a-z][a-z]|[a-z][a-z]-[A-Z][A-Z]|[a-z][a-z][a-z]|[a-z][a-z]-[A-Z][A-Z]-.*?)\\s*=\\s*(.*)")  # e.g. "de", "de-DE", "ast", "ca-ES-valencia"
+            match = regex.match(line)
+            if match:
+                codeToLang[match.group(1)] = match.group(2)
+        return codeToLang
 
 if len(sys.argv) != 4:
-    sys.stderr.write("Usage: " + sys.argv[0] + " <translationLangCode> <englishFile> <translatedFile>\n")
+    sys.stderr.write("Usage: {} <translationLangCode> <englishFile> <translatedFile>\n".format(sys.argv[0]))
     sys.exit()
 
 translationLangCode = sys.argv[1]
 translationLangCodeShort = re.sub('_.*', '', translationLangCode)
 if translationLangCodeShort == "el":
-    coreDictFile = "../languagetool/languagetool-language-modules/" + translationLangCodeShort + "/src/main/resources/org/languagetool/MessagesBundle_el_GR.properties"
+    coreDictFile = "../languagetool/languagetool-language-modules/{}/src/main/resources/org/languagetool/MessagesBundle_el_GR.properties".format(translationLangCodeShort)
 else:
-    coreDictFile = "../languagetool/languagetool-language-modules/" + translationLangCodeShort + "/src/main/resources/org/languagetool/MessagesBundle_" + translationLangCode + ".properties"
+    coreDictFile = "../languagetool/languagetool-language-modules/{0}/src/main/resources/org/languagetool/MessagesBundle_{1}.properties".format(translationLangCodeShort, translationLangCode)
 codeToLang = loadLanguageDict(coreDictFile)
 englishFile = open(sys.argv[2]).read()
-translatedFile = open(sys.argv[3])
-translatedJson = json.loads(translatedFile.read(), object_pairs_hook=collections.OrderedDict)
 newFile = englishFile
+translatedFile = open(sys.argv[3])
+translatedJson = json.loads(translatedFile.read(),
+                            object_pairs_hook=collections.OrderedDict)
 
 for k in translatedJson:
     translation = translatedJson[k]['message'].replace("\n", "\\\\n").replace("\"", "\\\"")
     backup = newFile
     quoteRegex = '"[^"\\\]*(?:\\\.[^"\\\]*)*"'    # see http://stackoverflow.com/questions/430759/
-    searchStr = '("' + k + '": {\\s*"message":\\s*' + quoteRegex + ')'
-    newFile = re.sub(searchStr, '"' + k + '": {\n    "message": "' + translation + '"', newFile, flags=re.MULTILINE|re.DOTALL)
+    searchStr = '(" {0} ": {\\s*"message":\\s* {1} )'.format(k, quoteRegex)
+    newFile = re.sub(searchStr,
+                     '"{0}": {\n    "message": "{1}"'.format(k, translation),
+                     newFile,
+                     flags=re.MULTILINE | re.DOTALL)
     if backup == newFile:
-        sys.stderr.write("WARN: Could not add translation '" + translation + "' for key '" + k + "', searched for: '" + searchStr + "'\n")
+        sys.stderr.write("WARN: Could not add translation '{0}' for key '{1}', searched for: '{2}'\n".format(translation, k, searchStr))
 
 newJson = json.loads(newFile, object_pairs_hook=collections.OrderedDict)
 for key in codeToLang:
     newKey = key.replace("-", "_")
     if newKey in newJson:
-        raise Exception("Cannot add key '" + newKey + "' to file, already exists")
+        raise Exception("Cannot add key '{}' to file, already exists".format(newKey))
     translatedLang = bytes(codeToLang[key], "utf-8").decode("unicode_escape")   # e.g. Franz\\u00f6sisch -> Französisch
-    newJson[newKey] = {'message': translatedLang, 'description': 'automatically added by injectTranslation.py'}
+    newJson[newKey] = {'message': translatedLang,
+                       'description': 'automatically added by injectTranslation.py'}
 
 print(json.dumps(newJson, indent=2, ensure_ascii=False, sort_keys=True))
