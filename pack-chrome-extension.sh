7802737879d60d58a641cab5e94a42708fc39b42
Daniel Naber <naber@danielnaber.de>
2016-11-14 10:33:45 +0100

    don't add node_modules to ZIP

--- pack-chrome-extension.sh
+++ pack-chrome-extension.sh
@@ -5,6 +5,6 @@ TARGET=$PWD/dist/languagetool-webextension-chrome.zip
 
 cd webextension
 rm -i $TARGET
-zip -x tests.js -x test/ -x test/\* -x languagetool-for-chrome-tests.html -r $TARGET .
+zip -x node_modules/\* -x tests.js -x test/ -x test/\* -x languagetool-for-chrome-tests.html -r $TARGET .
 echo "Saved to $TARGET"
 cd -
