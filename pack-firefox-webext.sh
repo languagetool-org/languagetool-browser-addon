666eadf56aacba40042bda397125139d81074cd5
Daniel Naber <naber@danielnaber.de>
2017-10-30 15:35:27 +0100

    remove 'offline_enabled' for Firefox, it's not supported and gives a warning on manual installation

--- pack-firefox-webext.sh
+++ pack-firefox-webext.sh
@@ -5,6 +5,8 @@ TARGET=$PWD/dist/languagetool-webextension-firefox.xpi
 
 cd webextension
 rm -i $TARGET
+sed -i.bak 's#"offline_enabled": false,##' manifest.json
 zip -x node_modules/\* -x tests.js -x test/ -x test/\* -x languagetool-for-chrome-tests.html -r $TARGET .
 echo "Saved to $TARGET"
+cp manifest.json.bak manifest.json
 cd -
